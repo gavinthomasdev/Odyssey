cd0126cc5568c98f03188856af9b5fe71d30b2e9
Gavin Thomas <gavin@gavinthomas.dev>
2022-12-20 10:31:18 -0500

    Layout

--- src/main/java/dev/gavinthomas/odyssey/ui/components/views/CenterView.java
+++ src/main/java/dev/gavinthomas/odyssey/ui/components/views/CenterView.java
@@ -1,7 +1,13 @@
 package dev.gavinthomas.odyssey.ui.components.views;
 
 import javax.swing.JPanel;
+import javax.swing.JTextPane;
 
 public class CenterView extends JPanel {
+
+}
+
+
+class Editor extends JTextPane {
 
 }
